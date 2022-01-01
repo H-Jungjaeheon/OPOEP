e3dd46b93990c1cc10f2075e470308d682936b13
H-Jungjaeheon <jjhking222@naver.com>
2022-01-01 20:40:18 +0900

    Stop
    Stop1

--- Assets/JaeHeon/Scripts/DWTest.cs
+++ Assets/JaeHeon/Scripts/DWTest.cs
@@ -7,6 +7,7 @@ public class DWTest : MonoBehaviour
 {
     [SerializeField] Image Background;
     [SerializeField] Image BackgroundBlack;
+    public bool Stop = false;
     void Start()
     {
         Background.transform.DOScale(0, 0).SetEase(Ease.OutBack);
@@ -19,9 +20,12 @@ public class DWTest : MonoBehaviour
     public void OnButtonClick()
     {     
         Background.transform.DOScale(1, 1).SetEase(Ease.OutBack);
+        Invoke("TimeStop", 0.8f);
     }
     public void OnExitButtonClick()
     {
+        Invoke("TimeStop", 0.8f);
+        Time.timeScale = 1;
         Background.transform.DOScale(0, 0.7f).SetEase(Ease.InBack);
     }
     public void OnButtonClickBlack()
@@ -31,5 +35,17 @@ public class DWTest : MonoBehaviour
     public void OnExitButtonClickBlack()
     {
         BackgroundBlack.transform.DOMove(new Vector3(-1090, 1000, 0), 0.8f).SetEase(Ease.InBack);
+    }
+    void TimeStop()
+    {
+        if (Stop == false)
+        {
+            Time.timeScale = 0;
+            Stop = true;
+        }
+        else if(Stop == true)
+        {
+            Stop = false;
+        }
     }
 }

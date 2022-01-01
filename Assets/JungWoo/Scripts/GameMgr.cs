ce1ff859b0bec68bf5e4bdb6a1f63c3862c13183
sketupcrobet <sketupcrobet@gmail.com>
2022-01-01 20:37:19 +0900

    거리마다 스폰

--- Assets/JungWoo/Scripts/GameMgr.cs
+++ Assets/JungWoo/Scripts/GameMgr.cs
@@ -4,40 +4,45 @@ using UnityEngine;
 
 public class GameMgr : MonoBehaviour
 {
-    public bool Buff1on, Buff2on, Buff3on;
-    public bool Shield;
-    public bool GameStart = false;
-    public GameObject Princessobj;
-    public GameObject StopButton;
+	public bool Buff1on, Buff2on, Buff3on;
+	public bool Shield;
+	public bool GameStart = false;
+	public GameObject Princessobj;
+	public GameObject StopButton;
+	public int SpawnInterval;
+	private Vector2 PrevLoc = new Vector2(0, 0);
 
-    void Start()
-    {
-        InvokeRepeating("Test", 1f, 1f);
-        if (Buff3on == true)
-        {
-            Shield = true;
-        }
-    }
+	void Start()
+	{
+		if (Buff3on == true)
+		{
+			Shield = true;
+		}
+	}
 
-    void Update()
-    {
+	void Update()
+	{
+		if (PrevLoc.y + SpawnInterval <= GameObject.Find("KingKong").gameObject.transform.position.y)
+		{
+			PrevLoc = GameObject.Find("KingKong").gameObject.transform.position;
+			SpawnPrincess();
+		}
+	}
 
-    }
-
-    void Test()
-    {
-        GameObject a = Instantiate(Princessobj, new Vector2(Random.Range(-2, 3), GameObject.Find("Main Camera").GetComponent<Transform>().position.y), Quaternion.identity);
-        if (Princess.princesscount >= 30)
-        {
-            a.GetComponent<SpriteRenderer>().color = new Color(0, 1, 0);
-        }
-        if (Princess.princesscount >= 75)
-        {
-            a.GetComponent<SpriteRenderer>().color = new Color(0, 0, 1);
-        }
-        if (Princess.princesscount >= 135)
-        {
-            Destroy(a);
-        }
-    }
+	void SpawnPrincess()
+	{
+		GameObject a = Instantiate(Princessobj, new Vector2(Random.Range(-2, 3), GameObject.Find("Main Camera").GetComponent<Transform>().position.y), Quaternion.identity);
+		if (Princess.princesscount >= 30)
+		{
+			a.GetComponent<SpriteRenderer>().color = new Color(0, 1, 0);
+		}
+		if (Princess.princesscount >= 75)
+		{
+			a.GetComponent<SpriteRenderer>().color = new Color(0, 0, 1);
+		}
+		if (Princess.princesscount >= 135)
+		{
+			Destroy(a);
+		}
+	}
 }
